487b2e8e1b1427b8d75e0de043b515c569b7c6ea
CuevaGavilanBenjamin <benjamin.cueva.g@uni.pe>
2025-05-20 01:16:12 -0500

    jeff es una prra

--- mi_proyecto_django/mi_proyecto_django/settings.py
+++ mi_proyecto_django/mi_proyecto_django/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'mi_proyecto_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'humedad_db',         # el nombre de tu base de datos
+        'USER': 'postgres',           # el usuario que creaste
+        'PASSWORD': '1234',  # la contraseña del usuario
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
